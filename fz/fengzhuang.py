89524a6dee7e95c1c53e910aa7314a39fcd7b031
dangai0201 <1471375117@qq.com>
2019-03-30 08:22:32 +0800

    添加chromedriver

--- fz/fengzhuang.py
+++ fz/fengzhuang.py
@@ -18,7 +18,7 @@ class Login():
 
 
         #self.driver = webdriver.Chrome()
-        self.driver = webdriver.Chrome('fz/chromedriver')
+        self.driver = webdriver.Chrome(executable_path='fz/chromedriver')
         self.driver.maximize_window()
         self.driver.get("http://192.168.43.129:811/ecshop/upload/")
         #print self.driver
